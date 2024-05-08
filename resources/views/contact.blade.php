d4b6fd7885e0365d5c531177754cc68150355dd1
AshishSubedi53 <ashishsubedi53@gmail.com>
2024-05-08 11:20:56 +0545

    whole project

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home Page</title>
   @vite('resources/css/app.css')
+  @vite('resources/js/app.js')
 </head>
 <body>
 @include('navbar')
@@ -12,10 +13,10 @@
     
     <div class="p-2">
       <h1 class="sm:text-center font-medium text-[20px] mb-2">Contact Us</h1>
-      <form action="{{route('form')}}" method="GET">
+      <form x-data="{isActive: false}" action="{{route('form')}}" method="GET">
         <div class="grid grid-row-4 gap-5">
           <div class="grid grid-cols-2 gap-2">
-            <input type="text" placeholder="First Name" name="firstName" class="border border-gray-500 focus:outline-none sm:px-4 sm:py-2">
+            <input :value='isActive' type="text" placeholder="First Name" name="firstName" class="border border-gray-500 focus:outline-none sm:px-4 sm:py-2">
             <input type="text" placeholder="Last Name" name="lastName" class="border border-gray-500 focus:outline-none sm:px-4 sm:py-2">
 
           </div>
@@ -23,7 +24,7 @@
           <input type="text" placeholder="Phone Number" name="phone" class="border border-gray-500 focus:outline-none sm:px-4 sm:py-2">
           <textarea cols="30" rows="10" name="message" placeholder="Write your message..." class="border border-gray-500 focus:outline-none sm:px-4 sm:py-2"></textarea>
           
-          <button type="submit" class="bg-blue-500 text-white font-2xl py-2">
+          <button @click="isActive = !isActive" :class="isActive ? 'bg-black' : ''" type="button" class="bg-blue-500 text-white font-2xl py-2">
             Submit
           </button>
         </div>
